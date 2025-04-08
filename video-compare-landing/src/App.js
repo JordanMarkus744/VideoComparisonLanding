46ed52dd915d7ba44fb8a8da918910717eeff062
Jordan <jordanmarkus7444@gmail.com>
2025-04-07 23:29:38 -0500

    changed from using next.js speed insights to react

--- video-compare-landing/src/App.js
+++ video-compare-landing/src/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import "./App.css";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
